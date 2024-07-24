53c5f1a237054d841cb04d92fdba72e17da29de8
pratikash <pratikashpanda.1234@gmail.com>
2024-07-24 19:06:18 +0530

    build command added

--- src/index.ts
+++ src/index.ts
@@ -10,13 +10,14 @@ mongoose
 
 const app = express();
 app.use(express.json());
-
 app.use(cors());
+
+app.get("/health", async (req: Request, res: Response) => {
+  res.send({ message: "health OK!" });
+});
 
 app.use("/api/my/user", myUserRoute);
 
-
 app.listen(7000, () => {
-    console.log("server started on port 7000");
-    
+    console.log("server started on port 7000"); 
 })
