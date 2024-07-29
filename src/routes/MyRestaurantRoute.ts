e2f4789441c8b36ffe26728e9330c554c2257866
pratikash <pratikashpanda.1234@gmail.com>
2024-07-29 16:45:25 +0530

    manage my restaurant feature

--- src/routes/MyRestaurantRoute.ts
+++ src/routes/MyRestaurantRoute.ts
@@ -31,4 +31,13 @@ router.post(
   MyRestaurantController.createMyRestaurant
 );
 
+router.put(
+  "/",
+  upload.single("imageFile"),
+  validateMyRestaurantRequest,
+  jwtCheck,
+  jwtParse,
+  MyRestaurantController.updateMyRestaurant
+);
+
 export default router;
